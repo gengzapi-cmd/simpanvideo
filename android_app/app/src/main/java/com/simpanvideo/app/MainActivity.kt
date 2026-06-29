e6fe137096861eb4381466a6fbf1296d10b30525
gengzapi-cmd <gengzapi@gmail.com>
2026-06-29 14:35:13 +0700

    Update MainActivity.kt

--- android_app/app/src/main/java/com/simpanvideo/app/MainActivity.kt
+++ android_app/app/src/main/java/com/simpanvideo/app/MainActivity.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
